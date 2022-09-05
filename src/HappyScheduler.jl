a579272570f80d680c82c0d13e927e6de21c40c8
kfrb <kai.friebi@gmail.com>
2022-09-05 16:24:28 +0200

    Export GenderSepRoomAllocationProblem

--- src/HappyScheduler.jl
+++ src/HappyScheduler.jl
@@ -8,7 +8,7 @@ using StatsBase: sample
 using ProgressMeter
 
 export get_guests, get_wishes, get_rooms
-export Guest, Wish, Room, RoomAllocationProblem
+export Guest, Wish, Room, RoomAllocationProblem, GenderSepRoomAllocationProblem
 export simulated_annealing
 export calc_room_id_of_guest, calc_guest_ids_of_room
 
