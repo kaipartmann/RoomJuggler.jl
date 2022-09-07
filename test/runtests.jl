b592e9bea966b10a0ea1bcf1811af258c57a8979
kfrb <kai.friebi@gmail.com>
2022-09-07 16:26:49 +0200

    Add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using HappyScheduler
 using Test
+using SafeTestsets
 
 @testset "HappyScheduler.jl" begin
-    # Write your tests here.
+    @safetestset "RoomAllocation" begin include(joinpath("room_allocation", "test_room_allocation.jl")) end
 end
