6f2942293c4a8553a06ce06aaf420839534a783f
Documenter.jl <documenter@juliadocs.github.io>
2023-02-07 09:27:32 +0000

    build based on 6f6a594

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RoomJuggler","category":"page"},{"location":"#RoomJuggler","page":"Home","title":"RoomJuggler","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RoomJuggler.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RoomJuggler]","category":"page"}]
+}
